8bb9985097b4dc9f9ca1c918daa85af6e08e73c3
Rain Babauta <rainbabauta1@gmail.com>
2020-07-29 17:41:43 -0700

    Added routes for editing and deleting users. Created a test file for app.py. Added edit-user.html file.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect
 from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -12,34 +12,71 @@ connect_db(app)
 db.create_all()
 
 @app.route("/")
+def redirct_to_users():
+  return redirect("/users")
+
+@app.route("/users")
 def list_users():
 
   users = User.query.all()
-  print (f"Users info {users}")
-  print (f"Users info {users[0]}")
+  # print (f"Users info {users}")
+  # print (f"Users info {users[0]}")
 
   return render_template("users.html", users=users)
 
-@app.route("/create-user")
+@app.route("/users/new")
 def show_user_form():
   return render_template("create-user.html")
 
 
-@app.route("/create-user", methods=["POST"])
+@app.route("/users/new", methods=["POST"])
 def create_user():
   first_name = request.form["first-name"]
   last_name = request.form["last-name"]
-  image_url = request.form["image-url"]
+  if request.form["image-url"]:
+    image_url = request.form["image-url"]
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+  else:
+    new_user = User(first_name=first_name, last_name=last_name)
 
-  new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
   db.session.add(new_user)
   db.session.commit()
 
   return redirect("/")
 
-@app.route("/user-detail/<username>")
+@app.route("/users/<username>")
 def show_user_details(username):
   user_details = User.query.get(username)
   print (f"this is the user image {user_details.image_url}")
   return render_template("user-detail.html", user_name=username, image_url = user_details.image_url)
 
+@app.route("/user/<username>/edit")
+def edit_user(username):
+  return render_template("edit-user.html")
+
+@app.route("/user/<username>/edit", methods=["POST"])
+def edit_user_form(username):
+  first_name = request.form["first-name"]
+  last_name = request.form["last-name"]
+  image_url = request.form["image-url"]
+
+  updated_user = User.query.get(username)
+  print(f"the new user is {updated_user}")
+  updated_user.first_name = first_name
+  updated_user.last_name = last_name
+  updated_user.image_url = image_url
+
+  db.session.commit()
+
+  return redirect("/")
+
+@app.route("/user/<username>/delete", methods=["POST"])
+def delete_user(username):
+  
+  delete_user = User.query.get(username)
+
+  db.session.delete(delete_user)
+
+  db.session.commit()
+
+  return redirect("/")
---
+++ test_flask.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+from app import app
+from models import db, User
+
+# Use test database and don't clutter tests with SQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
+app.config['SQLALCHEMY_ECHO'] = False
+
+# Make Flask errors be real errors, rather than HTML pages with error info
+app.config['TESTING'] = True
+
+# This is a bit of hack, but don't use Flask DebugToolbar
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+class UserViewsTestCase(TestCase):
+
+  def setUp(self):
+    """Add sample user."""
+
+    User.query.delete()
+
+    user = User(first_name='TestUser', last_name='Babauta')
+    db.session.add(user)
+    db.session.commit()
+
+    self.username = user.first_name
+
+  def tearDown(self):
+    """Clean up any fouled transaction."""
+
+    db.session.rollback()
+
+  def test_list_user(self):
+    with app.test_client() as client:
+      resp = client.get("/users")
+      html = resp.get_data(as_text=True)
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('TestUser', html)
+
+  def test_show_user(self):
+    with app.test_client() as client:
+      resp = client.get(f"/users/{self.username}")
+      html = resp.get_data(as_text=True)
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('<h1>TestUser</h1>', html)
+
+  def test_add_user(self):
+    with app.test_client() as client:
+      d = {"first-name": "TestUser2", "last-name": "genericlastname", "image-url": 'https://cdn.dribbble.com/users/2095589/screenshots/4166422/user_1.png'}
+      resp = client.post("/users/new", data=d, follow_redirects=True)
+      html = resp.get_data(as_text=True)
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('<li><a href="/users/TestUser2">TestUser2</a></li>', html)
