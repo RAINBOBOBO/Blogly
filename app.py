79312f9a31e38201476b51dbf1fdc2bf97029fa8
Shweta <arunbasappagudasi@Aruns-MacBook-Pro.local>
2020-07-30 17:29:11 -0700

    Added Doc strings and Test case for post

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request, redirect, flash
 from models import db, connect_db, User, Post
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'secretkey'
@@ -15,54 +15,69 @@ db.create_all()
 
 @app.route("/")
 def root():
+  ''' redirects to users page '''
+
   return redirect("/users")
 
 @app.route("/users")
 def list_users():
+  ''' Gets all the user details and pass it on to users page to list them  '''
 
   users = User.query.all()
-  # print (f"Users info {users}")
-  # print (f"Users info {users[0]}")
-
   return render_template("users.html", users=users)
 
 @app.route("/users/new")
 def show_user_form():
+  ''' Renders the create user page with a flash message to infrom the user '''
+
   flash(f"Adding user")
   return render_template("create-user.html")
 
 
 @app.route("/users/new", methods=["POST"])
 def create_user():
+  ''' Collects the inputs from the form and creates an instance of User by passing the details collected.
+      Adds instance to session and saves the same.
+      Redirects to users page '''
+
   first_name = request.form["first-name"]
   last_name = request.form["last-name"]
   image_url = request.form["image-url"] or None
+
   new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
   db.session.add(new_user)
   db.session.commit()
   return redirect("/users")
 
 @app.route("/users/<id>")
 def show_user_details(id):
+  '''Collects the User details and their posts and passes on the info to user details page '''
+
   user_details = User.query.get(id)
   user_posts = user_details.user_posts
-  print(f"this is the user posts {user_posts}")
   return render_template("user-detail.html", user_details=user_details, user_posts=user_posts)
 
 @app.route("/user/<id>/edit")
 def edit_user(id):
+  '''Gets the user details and passes it to edit user page with the flash message '''
+  
   user_details = User.query.get(id)
   flash(f"Editing details of {user_details.first_name}")
   return render_template("edit-user.html", user_details=user_details)
 
 @app.route("/user/<id>/edit", methods=["POST"])
 def edit_user_form(id):
+  '''Collects the updated user details from "Form" and 
+  gets the user instance and updates it with new values
+  and redirects to users route'''
+
   first_name = request.form["first-name"]
   last_name = request.form["last-name"]
   image_url = request.form["image-url"] or None
 
   updated_user = User.query.get(id)
-  print(f"the new user is {updated_user}")
+  
   updated_user.first_name = first_name
   updated_user.last_name = last_name
   updated_user.image_url = image_url
@@ -72,7 +87,9 @@ def edit_user_form(id):
 
 @app.route("/user/<int:id>/delete", methods=["POST"])
 def delete_user(id):
-  print (f"inside delete method")
+  '''gets the user id and deletes it from db 
+  and redirects to users route '''
+  
   delete_user = User.query.get(id)
 
   db.session.delete(delete_user)
@@ -83,12 +100,17 @@ def delete_user(id):
 
 @app.route("/users/<int:id>/posts/new")
 def show_create_post_form(id):
+  '''gets the user details, passes it to create post page and renders create post page'''
 
   user_details = User.query.get(id)
   return render_template("create-post.html", user_details=user_details)
 
 @app.route("/users/<int:id>/posts/new", methods=["POST"])
 def create_post(id):
+  '''Collects info from "Form" and creates instance of Post
+  and adds it to session and saves to db
+  redirects to users/id route '''
+
   title = request.form["post-title"]
   content = request.form["post-content"]
   new_post = Post(title=title, content=content, user_id=id)
@@ -100,19 +122,28 @@ def create_post(id):
 
 @app.route("/posts/<int:id>")
 def show_post(id):
+  '''Gets the details of post and passes it to post details page and renders the same '''
+
   post_details = Post.query.get(id)
   return render_template("post-detail.html", post_details=post_details)
 
 @app.route("/posts/<int:id>/edit")
 def show_edit_post(id):
+  '''Gets the user and post details and passes it to edit post page and renders the same'''
+
   post_details = Post.query.get(id)
   user_details = post_details.user_id
   return render_template(f"edit-post.html", user_details=user_details, post_details=post_details)
 
 @app.route("/posts/<int:id>/edit", methods=["POST"])
 def edit_post(id):
+  '''Collects the updated post details from "Form" and 
+  gets the post instance and updates it with new values
+  and redirects to posts/id route'''
+
   new_title = request.form["post-title"]
-  new_content = request.form["post-content"]
+  new_content = request.form["post-content"] 
+  
   post_details = Post.query.get(id)
   post_details.title = new_title
   post_details.content = new_content
@@ -123,6 +154,9 @@ def edit_post(id):
 
 @app.route("/posts/<int:id>/delete", methods=["POST"])
 def delete_post(id):
+  '''gets the user id and deletes it from db 
+  and redirects to users route '''
+
   print (f"inside delete method")
   delete_post = Post.query.get(id)
 
--- test_flask.py
+++ test_flask.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 # Use test database and don't clutter tests with SQL
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@localhost/blogly'
 app.config['SQLALCHEMY_ECHO'] = False
 
 # Make Flask errors be real errors, rather than HTML pages with error info
@@ -28,8 +28,8 @@ class UserViewsTestCase(TestCase):
     db.session.add(user)
     db.session.commit()
 
-    self.id = user.id
-    
+    self.user_id = user.id
+
   def tearDown(self):
     """Clean up any fouled transaction."""
 
@@ -45,17 +45,55 @@ class UserViewsTestCase(TestCase):
 
   def test_show_user(self):
     with app.test_client() as client:
-      resp = client.get(f"/users/{self.id}")
+      resp = client.get(f"/users/{self.user_id}")
       html = resp.get_data(as_text=True)
 
       self.assertEqual(resp.status_code, 200)
-      self.assertIn('<h1>TestUser</h1>', html)
+      self.assertIn('<h1 class="ml-auto mr-auto">TestUser Babauta</h1>', html)
 
   def test_add_user(self):
     with app.test_client() as client:
-      d = {2,"first-name": "TestUser2", "last-name": "genericlastname", "image-url": 'https://cdn.dribbble.com/users/2095589/screenshots/4166422/user_1.png'}
+      d = {"first-name": "TestUser2", "last-name": "genericlastname", "image-url": 'https://cdn.dribbble.com/users/2095589/screenshots/4166422/user_1.png'}
       resp = client.post("/users/new", data=d, follow_redirects=True)
       html = resp.get_data(as_text=True)
 
       self.assertEqual(resp.status_code, 200)
-      self.assertIn('<li><a href="/users/2">TestUser2</a></li>', html)
+      self.assertIn(f'<li><a href="/users/3">TestUser2</a></li>', html)
+      #How to get updated user id?
+  
+class PostViewsTestCase(TestCase):
+
+  def setUp(self):
+    """Add sample post."""
+
+    Post.query.delete()
+
+    user = User(first_name='TestUser', last_name='Babauta')
+    post = Post(title='Test title', content='This is complicated', user_id=user.id)
+    db.session.add(post)
+    db.session.commit()
+
+    self.post_user_id = post.id
+    self.user_user_id = user.id
+
+  def tearDown(self):
+    """Clean up any fouled transaction."""
+
+    db.session.rollback()
+
+  def test_create_post(self):
+    with app.test_client() as client:
+      Post.query.delete()
+      User.query.delete()
+
+      user = User(first_name='Shweta', last_name='Hosamani')
+      db.session.add(user)
+      db.session.commit()
+      
+      d = {"post-title": "new title by shweta", "post-content": "new content"}
+      resp = client.post(f"/users/{user.id}/posts/new", data=d, follow_redirects=True)
+      html = resp.get_data(as_text=True)
+      print (f"HTML HERE {html}")
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('<a href="/posts/2">new title by shweta</a>', html)
